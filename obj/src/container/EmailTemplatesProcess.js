54931a2a3107ff40a1b28f9fd67d7b4d2bd2e9ca
pipdeveloper <pipdevs@gmail.com>
2018-05-24 10:57:16 +0000

    project build by Travis CI [skip ci]

--- obj/src/container/EmailTemplatesProcess.js
+++ obj/src/container/EmailTemplatesProcess.js
@@ -1,11 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const pip_services_container_node_1 = require("pip-services-container-node");
+const pip_services_net_node_1 = require("pip-services-net-node");
+const pip_services_oss_node_1 = require("pip-services-oss-node");
 const EmailTemplatesServiceFactory_1 = require("../build/EmailTemplatesServiceFactory");
 class EmailTemplatesProcess extends pip_services_container_node_1.ProcessContainer {
     constructor() {
         super("email_templates", "Email templates microservice");
         this._factories.add(new EmailTemplatesServiceFactory_1.EmailTemplatesServiceFactory);
+        this._factories.add(new pip_services_net_node_1.DefaultNetFactory);
+        this._factories.add(new pip_services_oss_node_1.DefaultOssFactory);
     }
 }
 exports.EmailTemplatesProcess = EmailTemplatesProcess;
